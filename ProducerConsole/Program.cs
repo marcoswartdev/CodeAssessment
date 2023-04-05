1e86a3a187f1ab849e4cc08dda3235449502fb07
marco <marco@MARCOPC>
2023-04-05 18:19:22 +0200

    Add project files.

---
+++ ProducerConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ProducerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
