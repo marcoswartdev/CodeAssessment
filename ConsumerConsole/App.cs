612874e69f1179e136bdd7acb0624758084af1f3
marco <marco.swart.info@gmail.com>
2023-04-08 13:42:25 +0200

    Stage 4: Set up dependency injection container

---
+++ ConsumerConsole/App.cs
@@ -0,0 +1,19 @@
+﻿using Receiver.UseCases;
+
+namespace Receiver;
+
+public class App
+{
+    private readonly IReadMessageUseCase _readMessageUseCase;
+
+    public App(IReadMessageUseCase readMessageUseCase)
+    {
+        _readMessageUseCase = readMessageUseCase;
+    }
+
+    public void Run(string[] args)
+    {
+        _readMessageUseCase.Execute((message) => Console.WriteLine(message));
+        Console.ReadLine();
+    }
+}
--- ProducerConsole/Program.cs
+++ ProducerConsole/Program.cs
@@ -2,34 +2,35 @@
 using Sender.Repositories;
 using Sender.UseCases;
 using System.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MessageQueueLibrary.Interfaces;
+using Sender;
 
-namespace Sender;
+using IHost host = CreateHostBuilder(args).Build();
+using var scope = host.Services.CreateScope();
 
-internal class Program
+var services = scope.ServiceProvider;
+
+static IHostBuilder CreateHostBuilder(string[] args)
 {
-    static void Main(string[] args)
-    {
-        try
+    var connectionURL = ConfigurationManager.AppSettings["ConnectionURL"]!;
+    var queue = ConfigurationManager.AppSettings["QueueName"]!;
+    return Host.CreateDefaultBuilder(args).ConfigureServices((_, services) =>
         {
-            string connectionURL = ConfigurationManager.AppSettings["ConnectionURL"]!;
-            string queueName = ConfigurationManager.AppSettings["QueueName"]!;
-            SendMessageUseCase sendMessageUseCase = new SendMessageUseCase(
-                new SendMessageRepository(
-                    new Producer(
-                        new ConnectionWrapper(connectionURL), queueName)
-                    )
-                );
-            string name;
-            while (true)
-            {
-                Console.WriteLine("Please enter your name: ");
-                name = Console.ReadLine() ?? "";
-                sendMessageUseCase.Execute(name);
-            }
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine($"An error occured - {e.Message}");
-        }
-    }
+            services.AddSingleton<IConnectionWrapper>(x => new ConnectionWrapper(connectionURL));
+            services.AddSingleton<IProducer>(x => new Producer(x.GetRequiredService<IConnectionWrapper>(), queue));
+            services.AddSingleton<ISendMessageRepository, SendMessageRepository>();
+            services.AddSingleton<ISendMessageUseCase, SendMessageUseCase>();
+            services.AddSingleton<App>();
+        });
+}
+
+try
+{
+    services.GetRequiredService<App>().Run(args);
+}
+catch (Exception e)
+{
+    Console.WriteLine($"An error occured - {e.Message}");
 }
